7b37c630ea3249ca80af3271d4938840c610bed8
ChristodoulosPappas <39073996+ChristodoulosPappas@users.noreply.github.com>
2025-04-21 18:35:33 +0800

    Add files via upload

---
+++ alexnet_test.sh
@@ -0,0 +1,6 @@
+cd build/src
+./zkpot mAlexNet 4 1 4 1
+./zkpot mAlexNet 8 1 4 1
+./zkpot mAlexNet 16 1 4 1
+cd ..
+cd ..
---
+++ lenet_test.sh
@@ -0,0 +1,18 @@
+cd build/src
+./zkpot LENET 1 1 -1 1
+sleep 10
+./zkpot LENET 2 1 -1 1
+sleep 10
+./zkpot LENET 4 1 1 1
+sleep 10
+./zkpot LENET 8 1 1 1
+sleep 10
+./zkpot LENET 16 1 2 1
+sleep 10
+./zkpot LENET 32 1 2 1
+sleep 10
+./zkpot LENET 64 1 2 1
+
+
+cd ..
+cd ..
